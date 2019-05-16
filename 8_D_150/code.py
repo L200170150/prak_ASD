16019e18bd6159e4f80583ac5cbe100eac34eba8
L200170150 <47962565+L200170150@users.noreply.github.com>
2019-05-16 09:43:51 +0700

    Add files via upload

---
+++ 8_D_150/code.py
@@ -0,0 +1,15 @@
+class Stack():
+    def __init__(self):
+        self.items = []
+    def isEmpty(self):
+        return len(self)==0
+    def __len__(self):
+        return len(self.items)
+    def peek(self):
+        assert not self.isEmpty()
+        return self.items[-1]
+    def pop(self):
+        assert not self.isEmpty()
+        return self.items.pop()
+    def push(self, data):
+        return self.items.append(data)
---
+++ 8_D_150/no2.py
@@ -0,0 +1,6 @@
+from code import Stack
+nilai = Stack()
+for i in range(16):
+    if i%3 == 0:
+        nilai.push(i)
+        print(nilai.items)
